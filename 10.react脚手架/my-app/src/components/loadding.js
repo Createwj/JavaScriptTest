33f0012acb719e77272f30ec5e9abcbf91de3d0b
“jiang.wang” <wangj9845@163.com>
2018-05-08 10:24:32 +0800

    react脚手架

---
+++ 10.react脚手架/my-app/src/components/loadding.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+class loadding extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <h1>1234567</h1>
+      </div>
+    );
+  }
+}
+
+export default loadding;
