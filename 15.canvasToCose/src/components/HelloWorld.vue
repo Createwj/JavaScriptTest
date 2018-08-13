dfb9a3b21d223b1780d862fe6120413ff55d4071
“jiang.wang” <wangj9845@163.com>
2018-08-13 11:37:15 +0800

    将远程图片直接转换成base64 返回

--- 15.canvasToCose/src/components/HelloWorld.vue
+++ 15.canvasToCose/src/components/HelloWorld.vue
@@ -44,6 +44,26 @@ export default {
           document.body.appendChild(canvas)
           }
         );
+          this.convertImgToBase64('https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3497300994,2503543630&fm=27&gp=0.jpg', function(base64Img){
+            console.log(base64Img)
+          });
+  },
+  methods:{
+    convertImgToBase64(url, callback, outputFormat){
+        var canvas = document.createElement('CANVAS'),
+          ctx = canvas.getContext('2d'),
+          img = new Image;
+        img.crossOrigin = 'Anonymous';
+        img.onload = function(){
+          canvas.height = img.height;
+          canvas.width = img.width;
+          ctx.drawImage(img,0,0);
+          var dataURL = canvas.toDataURL(outputFormat || 'image/png');
+          callback.call(this, dataURL);
+          canvas = null;
+        };
+        img.src = url;
+      }
   }
 }
 </script>
